aaf53791532f477fd1f0c0b9461e8684ffe592c6
journeyman846 <jamescrafton@live.com>
2019-02-25 18:44:35 -0600

    Added drop down

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class MyTextInput extends StatefulWidget {
@@ -7,8 +6,8 @@ class MyTextInput extends StatefulWidget {
 }
 
 class MyTextInputState extends State<MyTextInput> {
-  
   String nickname = "";
+  String dropdownValue;
   void onPressed() {
     print('Button Pressed');
   }
@@ -19,7 +18,7 @@ class MyTextInputState extends State<MyTextInput> {
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(
-        title: new Text("Profile"), backgroundColor: Colors.blueGrey),
+            title: new Text("Profile"), backgroundColor: Colors.blueGrey),
         body: new Container(
           padding: new EdgeInsets.all(32.0),
           child: new Center(
@@ -27,9 +26,8 @@ class MyTextInputState extends State<MyTextInput> {
               children: <Widget>[
                 new TextField(
                   decoration: new InputDecoration(
-                    icon: new Icon(Icons.account_circle),
-                    labelText: 'Enter your nickname'
-                  ),
+                      icon: new Icon(Icons.account_circle),
+                      labelText: 'Enter your nickname'),
                   onChanged: (String value) {
                     onChanged(value);
                     setState(() {
@@ -37,14 +35,25 @@ class MyTextInputState extends State<MyTextInput> {
                     });
                   },
                 ),
-                // new CheckboxListTile(),
                 new RaisedButton(
                   child: new Text('Submit'),
                   onPressed: () {
                     onPressed();
                   },
                 ),
-                new Text(nickname)
+                DropdownButton<String>(
+                    value: dropdownValue,
+                    onChanged: (String newValue) {
+                      setState(() {
+                        if (newValue != null) dropdownValue = newValue;
+                      });
+                    },
+                    hint: Text("Please select your age range"),
+                    items: <String>['13-15', '15-17', '17-19', '19-21']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                          value: value, child: Text(value));
+                    }).toList())
               ],
             ),
           ),
