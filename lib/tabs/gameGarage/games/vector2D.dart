7fe73f80364fb6f27c0bfe89dc5b3f70bfed9402
AndrewH <andrewrhands@gmail.com>
2019-03-25 18:25:28 -0500

    started golf game

---
+++ lib/tabs/gameGarage/games/vector2D.dart
@@ -0,0 +1,16 @@
+import 'dart:math';
+
+import 'package:flame/position.dart';
+class Vector2D {
+  double x,y;
+  Vector2D(this.x,this.y);
+  Vector2D operator +(Vector2D b) => Vector2D(x+b.x,y+b.y);
+  Vector2D operator -(Vector2D b) => Vector2D(x-b.x,y-b.y);
+  Vector2D operator *(double b) => Vector2D(x*b,y*b);
+  Vector2D operator /(double b) => Vector2D(x/b,y/b);
+  Position tpPosition() => Position(x,y);
+  double dot(Vector2D b) => x*b.x+y*b.y;
+  double cross(Vector2D b) => x*b.y-y*b.x;
+  //angle is in radians
+  Vector2D fromAngle(double angle) => Vector2D(cos(angle),sin(angle));
+}
