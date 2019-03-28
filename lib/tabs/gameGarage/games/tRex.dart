e074f4e6928c06bfcd764ccfdea1ea9216119f49
arhands <andrewrhands@gmail.com>
2019-03-27 20:30:26 -0500

    fixed trex game (background clouds still broke)

--- lib/tabs/gameGarage/games/tRex.dart
+++ lib/tabs/gameGarage/games/tRex.dart
@@ -3,12 +3,22 @@ import 'dart:ui';
 import 'package:flame/components/component.dart';
 //import 'package:flame/flame.dart' as ui;
 import 'package:flame/flame.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:tobaccoFreeAmarilloApp/game/game.dart';
 import 'package:tobaccoFreeAmarilloApp/game/trexgame.dart';
 
 import 'genericGame.dart';
 import 'package:tobaccoFreeAmarilloApp/game/trexgame.dart';
+
+import 'dart:ui' as ui;
+
+import 'package:flame/flame.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart' as material;
+import 'package:flutter/services.dart';
+import 'package:tobaccoFreeAmarilloApp/game/game.dart';
+
 class TRex extends GenericGame
 {
   TRexGame tRexGame;
@@ -19,9 +29,17 @@ class TRex extends GenericGame
   //TRexGame tRexGame = TRexGame(spriteImage: image[0]);
   Flame.images.loadAll(["sprite.png"]).then((List<Image> image){
     tRexGame = TRexGame(spriteImage: image[0]);
-  });
+    });
+    Flame.audio.disableLog();
+    //List<ui.Image> image = await Flame.images.loadAll(["sprite.png"]);
+    //TRexGame tRexGame = TRexGame(spriteImage: image[0]);
+
+    Flame.util.addGestureRecognizer(new TapGestureRecognizer()
+      ..onTapDown = (TapDownDetails evt) => tRexGame.onTap());
+
+    SystemChrome.setEnabledSystemUIOverlays([]);
   }
   @override
   // TODO: implement widget
-  Widget get widget => GameWrapper(tRexGame);
+  Widget get widget => material.Material(child: GameWrapper(tRexGame));
 }
