c3d243e913767ba68073a28a3b07175bac1fa392
arhands <andrewrhands@gmail.com>
2019-03-27 20:48:07 -0500

    added graphics fetching

--- lib/tabs/gameGarage/games/cannon.dart
+++ lib/tabs/gameGarage/games/cannon.dart
@@ -7,13 +7,36 @@ import 'package:flame/position.dart';
 import 'package:flame/sprite.dart';
 import 'package:flutter/material.dart';
 import 'vector2D.dart';
+import 'package:flame/flame.dart';
 class Cannon extends Component {
   Cannon(){
+    if(!Flame.images.loadedFiles.containsKey("CannonStand.png"))
+      Flame.images.load("CannonStand.png");
+    if(!Flame.images.loadedFiles.containsKey("CannonBarrel.png"))
+      Flame.images.load("CannonBarrel.png");
 
+    _cannonStand = SpriteComponent.fromSprite(64, 64, 
+      Sprite.fromImage(Flame.images.loadedFiles["CannonStand.png"],
+        x: 0,
+        y: 0,
+        width: 64,
+        height: 64));
+    _cannonStand.width = 64;
+    _cannonStand.height = 64;
+
+    _cannonBarrel = SpriteComponent.fromSprite(64, 64, 
+      Sprite.fromImage(Flame.images.loadedFiles["CannonBarrel.png"],
+        x: 0,
+        y: 0,
+        width: 64,
+        height: 64));
+    _cannonBarrel.width = 64;
+    _cannonBarrel.height = 64;
   }
+  
   bool active = true;
-  SpriteComponent _stand;
-  SpriteComponent _barrel;
+  SpriteComponent _cannonStand;
+  SpriteComponent _cannonBarrel;
   @override
   void render(Canvas c) {
     // TODO: implement render
